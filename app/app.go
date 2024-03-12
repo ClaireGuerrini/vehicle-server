a331a235df695bf6e133b938c98950e87919e6cd
Claire Guerrini <claire.guerrini2@hotmail.fr>
2024-03-12 08:42:28 +0000

    dependances

--- app/app.go
+++ app/app.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/cicd-lectures/vehicle-server/storage"
-	"github.com/cicd-lectures/vehicle-server/vehicle"
+	"github.com/ClaireGuerrini/vehicle-server/storage"
+	"github.com/ClaireGuerrini/vehicle-server/vehicle"
 	"go.uber.org/zap"
 )
 
--- app/app_test.go
+++ app/app_test.go
@@ -7,10 +7,10 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/cicd-lectures/vehicle-server/pkg/httputil"
-	"github.com/cicd-lectures/vehicle-server/pkg/testutil"
-	"github.com/cicd-lectures/vehicle-server/storage/vehiclestore"
-	"github.com/cicd-lectures/vehicle-server/vehicle"
+	"github.com/ClaireGuerrini/vehicle-server/pkg/httputil"
+	"github.com/ClaireGuerrini/vehicle-server/pkg/testutil"
+	"github.com/ClaireGuerrini/vehicle-server/storage/vehiclestore"
+	"github.com/ClaireGuerrini/vehicle-server/vehicle"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
